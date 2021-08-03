c4579623f862b071a0b9e380a21cf4c97a5856c6
Twin-2 <davidwhit818@gmail.com>
2021-08-03 11:55:07 -0700

    finished backend routes

--- back-end/modules/UserRoutes.js
+++ back-end/modules/UserRoutes.js
@@ -3,12 +3,14 @@
 const getKey = require('../lib/getKey.js');
 const jwt = require('jsonwebtoken');
 const User = require('../schemas/userSchema.js');
+const axios = require('axios');
+const userSchema = require('../schemas/userSchema.js');
 
 const Account = {}
 
 Account.profile = (req, res) => {
   const token = req.headers.authorization.split(' ')[1];
-  jwt.verify(token, getKey, {}, function(err, user) {
+  jwt.verify(token, getKey, {}, function (err, user) {
     if (err) {
       res.send('invalid token - you cannot access this route');
     } else {
@@ -18,25 +20,52 @@ Account.profile = (req, res) => {
   console.log('profile page');
 }
 
-Account.listOfExercises = (req, res) => {
-  axios.get('https://wger.de/api/v2/exerciseinfo')
-  .then(result => res.send(result.data.results))
+Account.listOfExercises = async (req, res) => {
+  await axios.get('https://wger.de/api/v2/exerciseinfo/?limit=200')
+    .then(result => {
+      // console.log(result.data.results)
+      let exerciseData = result.data.results.map(value => {
+        let equipment = value.equipment.map(item => item.name)
+        let exercise =
+        {
+          'name': value.name,
+          'description': value.description,
+          'category': value.category.name,
+          'equipment': equipment
+        }
+        return exercise
+      })
+      // console.log(exerciseData)
+      res.send(exerciseData)
+    })
 }
 
 Account.saveExercise = async (req, res) => {
   const { email, username, name, description, category, equipment } = req.body;
-  const newExercise = {name, description, category, equipment};
-  await User.findOne({"email": email}, (err, user) =>{
+  const newExercise = { name, description, category, equipment };
+  await User.findOne({ "email": email }, (err, user) => {
     if (user) {
       user.exercises.push(newExercise);
       user.save()
-      .then(() => {res.send(user.exercises)})
+        .then(() => { res.send(user.exercises) })
     } else {
-      let newUser = new User({ email, username, 'exercises':[newExercise]})
+      let newUser = new User({ email, username, 'exercises': [newExercise] })
       newUser.save()
-      .then(() => {res.send(user.exercises)})
-    }  
+        .then(() => { res.send(user.exercises) })
+    }
   })
+}
+
+Account.deleteExercise = async (req, res) => {
+  let id = req.query.id;
+  let email = req.query.email;
+  await User.findOne({ 'email': email })
+    .then(user => {
+      let filteredExercises = user.exercises.filter((value) => value.id !== id)
+      user.exercises = filteredExercises;
+      user.save();
+      res.send(user.exercises)
+    })
 }
 
 module.exports = Account;
--- back-end/server.js
+++ back-end/server.js
@@ -22,7 +22,7 @@ mongoose.connect(process.env.MONGODB_URI, mongooseOptions)
 app.get('/profile', Account.profile);
 app.get('/exercise', Account.listOfExercises);
 app.post('/exercise', Account.saveExercise);
-// app.delete('/exercise', User.deleteExercise);
+app.delete('/exercise', Account.deleteExercise);
 
 
 app.use('*', (req, res) => {
